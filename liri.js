60e0118368430976c6145f44755f54826b3e2354
jmona789 <jmona789@gmail.com>
2016-01-15 00:20:10 -0500

    added variables and switch case statement

--- liri.js
+++ liri.js
@@ -1,1 +1,18 @@
+var fs = require("fs");
 var keys = require("./keys.js");
+var param = process.argv.slice(2);
+
+switch(param[0]){
+  case "my-tweets":
+    myTweets();
+    break;
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
+  case "movie-this":
+    movieThis();
+    break;
+  case "do-what-it-says":
+   doWhatItSays();
+   break;
+}
