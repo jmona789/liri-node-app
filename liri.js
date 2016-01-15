6245c58248bf8b53c032d826748982148b5e3f26
jmona789 <jmona789@gmail.com>
2016-01-15 00:52:47 -0500

    made myTweets() function that displays the users last 20 tweets

--- liri.js
+++ liri.js
@@ -2,6 +2,15 @@ var fs = require("fs");
 var keys = require("./keys.js");
 var param = process.argv.slice(2);
 
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret
+});
+
 switch(param[0]){
   case "my-tweets":
     myTweets();
@@ -15,4 +24,18 @@ switch(param[0]){
   case "do-what-it-says":
    doWhatItSays();
    break;
+}
+
+function myTweets(){
+  client.get('statuses/user_timeline', function(error, tweets, response){
+    console.log(error);
+    if(error) {
+      console.log(error);
+      throw error;
+    }
+    console.log("Your last 20 tweets:")
+    for (i = 0; i < 20; i++){
+      console.log(i+1 + ". On " + tweets[i].created_at + " You Tweeted: " + tweets[i].text);
+    }
+  });
 }
